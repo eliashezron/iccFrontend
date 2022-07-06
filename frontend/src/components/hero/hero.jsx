b58def5d239ce6e525b34b10bea7544730acbd2a
eliashezron <eliashezron23@gmail.com>
2022-07-06 17:57:22 +0000

    hero section

--- frontend/src/components/hero/hero.jsx
+++ frontend/src/components/hero/hero.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import './hero.scss';
+import sidebar from '../../assets/sidebar.svg'
+import sidebarhover from "../../assets/sidebarhover.svg"
+import gifimage from "../../assets/gifimage.png"
+import logo from "../../assets/logo.png"
+import playnow from "../../assets/playnow.svg"
+import playnowhover from "../../assets/playnowhover.svg"
 
 export default function Hero(){
     return (
-        <div>
-        <h1>hero</h1>
+        <div className='heroContainer'>
+            <div className='theorySection'>
+                <div className='logoImage'>
+                    <img src={logo} alt='logo'/>
+                </div>
+                <div className='textfield'>
+                <p>Intergalactic Cockroach Space Race is an esport game based on blockchain technology. 
+                    You can gather your friends, form a team, battle with others and earn crypto with just your skills
+                    #IntergalacticCockroach #ICC #SpaceRace # P2E #NFT
+                </p>
+                <p>$ICC Contract Address</p>
+                <input placeholder='BEP20
+                0x8d672014fb107cb409dccd9042dda3b97313f4c3'></input>
+                </div>
+                <div className='playButton'>
+                    <img src={playnow} alt="playnow"
+                    onMouseOver={e => (e.currentTarget.src = playnowhover)}
+                    onMouseOut={e => (e.currentTarget.src = playnow)}/>
+                </div>
+            </div>
+            <div className='gifSection'>
+                <img src={gifimage} alt="gifimage"/>
+            </div>
+            <div className='socialIcons'>
+            <img src={sidebar} alt="playnow"
+                onMouseOver={e => (e.currentTarget.src = sidebarhover)}
+                onMouseOut={e => (e.currentTarget.src = sidebar)}/>
+            </div>
         </div>
     )
 }
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './navbar.scss';
+import React from "react"
+import "./navbar.scss"
+import logo from '../../assets/logo.png'
+import playnow from "../../assets/playnow.svg"
+import playnowhover from "../../assets/playnowhover.svg"
 
-export default function NavBar(){
-    return (
-        <div>
-        <h1>navbar</h1>
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navcontainer'>
+        <div className='logo'>
+          <img src={logo} alt='logo' />
         </div>
-    )
+        <div className='navlinks'>
+            <ul>
+            <li>
+                <a href='#home'>Home</a>
+            </li>
+            <li>
+                <a href='#nft'>Nft</a>
+            </li>
+            <li>
+                <a href='#earn'>Earn</a>
+            </li>
+            <li>
+                <a href='#trade'>Trade</a>
+            </li>
+            <li>
+                <a href='#More'>More</a>
+            </li>
+            </ul>
+            <div className='playButton'>
+                <img src={playnow} alt="playnow"
+                onMouseOver={e => (e.currentTarget.src = playnowhover)}
+                onMouseOut={e => (e.currentTarget.src = playnow)}/>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Navbar
