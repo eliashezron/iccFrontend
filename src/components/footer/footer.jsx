5235bda496a9586a2a5094b8098df9988a8e1945
eliashezron <eliashezron23@gmail.com>
2022-07-10 15:29:23 +0300

    v1.02

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,84 @@
-import React from 'react';
-import './footer.scss';
-
-export default function Footer(){
-    return (
-        <div>
-        <h1>footer</h1>
+import React from "react"
+import "./footer.scss"
+import logo from "../../assets/logo.png"
+import opensea from "../../assets/opensea.png"
+import discord from "../../assets/discord.png"
+import twitter from "../../assets/twitter.png"
+import mintnow from "../../assets/mintnow.svg"
+import mintnowhover from "../../assets/mintnowhover.svg"
+import playnow from "../../assets/playnowlong.svg"
+import playnowhover from "../../assets/playnowlonghover.svg"
+import iccprice from "../../assets/iccprice.svg"
+import buyicca from "../../assets/buyicca.svg"
+import buyiccahover from "../../assets/buyiccahover.svg"
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div className='seca'>
+        <div className='footerimg'>
+          <img src={logo} alt='logo' />
         </div>
-    )
+        <div className='footerbuttons'>
+          <button>Home</button>
+          <button>Earn</button>
+          <button>Trade</button>
+          <button>WhitePaper</button>
+          <button>Farms</button>
+          <button>Exchange</button>
+          <button>Nfts</button>
+          <button>Pools</button>
+          <button>Liquidity</button>
+        </div>
+        <div className='community'>
+          <div className='bgc'>
+            <p>Join our community</p>
+            <div className='communitybuttons'>
+              <a href='#home'>
+                <img src={opensea} alt='opensea' />
+              </a>
+              <a href='#home'>
+                <img src={discord} alt='discord' />
+              </a>
+              <a href='#home'>
+                <img src={twitter} alt='twitter' />
+              </a>
+            </div>
+          </div>
+          <button>contact us</button>
+        </div>
+      </div>
+      <div className='secb'>
+        <div className='playbutton'>
+          <img
+            src={playnow}
+            alt='playnow'
+            onMouseOver={(e) => (e.currentTarget.src = playnowhover)}
+            onMouseOut={(e) => (e.currentTarget.src = playnow)}
+          />
+        </div>
+        <div className='mint'>
+          <img
+            src={mintnow}
+            alt='playnow'
+            onMouseOver={(e) => (e.currentTarget.src = mintnowhover)}
+            onMouseOut={(e) => (e.currentTarget.src = mintnow)}
+          />
+        </div>
+      </div>
+      <div className='secc'>
+        <div className='h1'>
+          2022@ INTERGALACTIC COCKROACH GAME. ALL RIGHTS RESERVED.
+        </div>
+        <div className='h2'>
+          <img src={iccprice} alt='icc price' />
+          <img
+            src={buyicca}
+            alt='buyicca'
+            onMouseOver={(e) => (e.currentTarget.src = buyiccahover)}
+            onMouseOut={(e) => (e.currentTarget.src = buyicca)}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,16 @@ import "./navbar.scss"
 import logo from "../../assets/logo.png"
 import playnow from "../../assets/playnow.svg"
 import playnowhover from "../../assets/playnowhover.svg"
+import home from "../../assets/ihomei.svg"
+import homehover from "../../assets/ihomehoveri.svg"
+import earn from "../../assets/iearni.svg"
+import earnhover from "../../assets/iearnhoveri.svg"
+import trade from "../../assets/itrade.svg"
+import tradehover from "../../assets/itradehover.svg"
+import nft from "../../assets/infti.svg"
+import nfthover from "../../assets/infthoveri.svg"
+import more from "../../assets/imore.svg"
+import morehover from "../../assets/imorehover.svg"
 
 const Navbar = () => {
   return (
@@ -14,19 +24,54 @@ const Navbar = () => {
         <div className='navlinks'>
           <ul>
             <li>
-              <a href='#home'>Home</a>
+              <a href='#home'>
+                <img
+                  src={home}
+                  alt='home'
+                  onMouseOver={(e) => (e.currentTarget.src = homehover)}
+                  onMouseOut={(e) => (e.currentTarget.src = home)}
+                />
+              </a>
             </li>
             <li>
-              <a href='#nft'>Nft</a>
+              <a href='#nft'>
+                <img
+                  src={nft}
+                  alt='nft'
+                  onMouseOver={(e) => (e.currentTarget.src = nfthover)}
+                  onMouseOut={(e) => (e.currentTarget.src = nft)}
+                />
+              </a>
             </li>
             <li>
-              <a href='#earn'>Earn</a>
+              <a href='#earn'>
+                <img
+                  src={earn}
+                  alt='earn'
+                  onMouseOver={(e) => (e.currentTarget.src = earnhover)}
+                  onMouseOut={(e) => (e.currentTarget.src = earn)}
+                />
+              </a>
             </li>
             <li>
-              <a href='#trade'>Trade</a>
+              <a href='#trade'>
+                <img
+                  src={trade}
+                  alt='trade'
+                  onMouseOver={(e) => (e.currentTarget.src = tradehover)}
+                  onMouseOut={(e) => (e.currentTarget.src = trade)}
+                />
+              </a>
             </li>
             <li>
-              <a href='#More'>More</a>
+              <a href='#More'>
+                <img
+                  src={more}
+                  alt='more'
+                  onMouseOver={(e) => (e.currentTarget.src = morehover)}
+                  onMouseOut={(e) => (e.currentTarget.src = more)}
+                />
+              </a>
             </li>
           </ul>
           <div className='playButton'>
