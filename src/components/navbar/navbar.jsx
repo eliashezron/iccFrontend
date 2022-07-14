fd64a3e085f35fb927fc14bc7ad26186a934e67b
eliashezron <eliashezron23@gmail.com>
2022-07-14 11:18:21 +0300

    deploy

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,52 +1,52 @@
-import React, { useState } from 'react';
-import './navbar.scss';
-import logo from '../../assets/logo.png';
-import playnow from '../../assets/playnow.svg';
-import playnowhover from '../../assets/playnowhover.svg';
-import { navItems } from '../../data/navItems';
+import React, { useState } from "react"
+import "./navbar.scss"
+import logo from "../../assets/logo.png"
+import playnow from "../../assets/playnow.svg"
+import playnowhover from "../../assets/playnowhover.svg"
+import { navItems } from "../../data/navItems"
 
 const Navbar = () => {
-  const [dropdown, setDropdown] = useState(false);
+  const [dropdown, setDropdown] = useState(false)
   return (
-    <div className="navbar">
-      <div className="navcontainer">
-        <div className="logo">
-          <img src={logo} alt="logo" />
+    <div className='navbar'>
+      <div className='navcontainer'>
+        <div className='logo'>
+          <img src={logo} alt='logo' />
         </div>
-        <div className="navlinks">
+        <div className='navlinks'>
           <ul>
             {navItems.map((item) => (
-              <div className={`dropdown ${dropdown && 'active'}`} key={item.id}>
+              <div className={`dropdown ${dropdown && "active"}`} key={item.id}>
                 <button
                   key={item.id}
                   onClick={() => setDropdown(true)}
                   onMouseLeave={() => setDropdown(false)}
-                  className={`${dropdown ? 'active' : 'button'}`}
+                  className={`${dropdown ? "active" : "button"}`}
                 >
                   {item.title}
                 </button>
-                {item.title === 'Earn' ? (
-                  <div className="dropdown-content">
+                {item.title === "Earn" ? (
+                  <div className='dropdown-content'>
                     <a href={item.path} key={item.id}>
-                      pools
+                      POOLS
                     </a>
                     <a href={item.path} key={item.id}>
-                      farms
-                    </a>
-                  </div>
-                ) : item.title === 'Trade' ? (
-                  <div className="dropdown-content">
-                    <a href={item.path} key={item.id}>
-                      Liquidity
-                    </a>
-                    <a href={item.path} key={item.id}>
-                      Exchange
+                      FARMS
                     </a>
                   </div>
-                ) : item.title === 'More' ? (
-                  <div className="dropdown-content">
+                ) : item.title === "Trade" ? (
+                  <div className='dropdown-content'>
                     <a href={item.path} key={item.id}>
-                      WhitePaper
+                      LIQUIDITY
+                    </a>
+                    <a href={item.path} key={item.id}>
+                      EXCHANGE
+                    </a>
+                  </div>
+                ) : item.title === "More" ? (
+                  <div className='dropdown-content'>
+                    <a href={item.path} key={item.id}>
+                      WHITEPAPER
                     </a>
                     <br />
                   </div>
@@ -54,10 +54,10 @@ const Navbar = () => {
               </div>
             ))}
           </ul>
-          <div className="playButton">
+          <div className='playButton'>
             <img
               src={playnow}
-              alt="playnow"
+              alt='playnow'
               onMouseOver={(e) => (e.currentTarget.src = playnowhover)}
               onMouseOut={(e) => (e.currentTarget.src = playnow)}
             />
@@ -65,7 +65,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
