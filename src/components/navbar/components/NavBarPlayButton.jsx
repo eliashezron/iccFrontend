88a1d56b9f1a0131fd91f1948caaeaa6c1f0955a
Kimaswa Emmanuel Yusufu <kimaswaemma36@gmail.com>
2022-07-18 17:37:50 +0300

    remv

--- src/components/navbar/components/NavBarPlayButton.jsx
+++ src/components/navbar/components/NavBarPlayButton.jsx
@@ -1,38 +1,6 @@
 import * as React from 'react';
-import playnow from '../../../assets/playnow.svg';
-import playnowhover from '../../../assets/playnowhover.svg';
 import './navbar-play-button.scss';
 
 export default function NavBarPlayButton() {
-  const [hovered, setHovered] = React.useState();
-
-  return (
-    <div id="navbar-play-button" />
-    // <div
-    //   onMouseOver={() => setHovered(true)}
-    //   onMouseOut={() => setHovered(false)}
-    //   className={`tw-relative tw-cursor-pointer 3xl:tw-w-[338px] 3xl:tw-h-[69px] tw-w-[300px] tw-h-[50px]`}
-    // >
-    //   <img
-    //     src={playnowhover}
-    //     alt=""
-    //     style={{
-    //       transitionDelay: '200ms',
-    //     }}
-    //     className={`tw-w-full tw-h-full tw-absolute tw-ease-in tw-transition-opacity ${
-    //       hovered ? 'tw-opacity-100' : 'tw-opacity-0'
-    //     }`}
-    //   />
-    //   <img
-    //     src={playnow}
-    //     alt=""
-    //     style={{
-    //       transitionDelay: '200ms',
-    //     }}
-    //     className={`tw-w-full tw-h-full tw-absolute tw-ease-in tw-transition-opacity ${
-    //       hovered ? 'tw-opacity-0' : 'tw-opacity-100'
-    //     }`}
-    //   />
-    // </div>
-  );
+  return <div id="navbar-play-button" />;
 }
