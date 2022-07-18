58cd4afec1bbda3b283b36b18f405215170dd528
Kimaswa Emmanuel Yusufu <kimaswaemma36@gmail.com>
2022-07-18 11:11:49 +0300

    updated hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -10,8 +10,10 @@ import earn from '../../assets/earnicon.png';
 import trade from '../../assets/tradeicon.png';
 import more from '../../assets/moreicon.png';
 import nft from '../../assets/nfticon.png';
-// import playNow from '../../assets/playnowidle.png';
-// import playNowidlehover from '../../assets/playnowidlehover.png';
+import logo from '../../assets/logo.png';
+import footerlogoicon from '../../assets/footerlogoicon.png';
+import playNow from '../../assets/playnowidle.png';
+import playNowidlehover from '../../assets/playnowidlehover.png';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 import NavBar from '../navbar/navbar';
@@ -30,10 +32,13 @@ export default function Hero() {
     <section id="hero">
       <NavBar />
       <div id="hero">
-        <div className="tw-flex lg:tw-flex-row tw-w-full tw-h-[62vh] xl:tw-h-[66vh] 2xl:tw-h-[59.5vh] 3xl:tw-h-[63vh] tw-gap-10">
+        <div className="tw-flex lg:tw-flex-row tw-w-full tw-h-[62vh] xl:tw-h-[66vh] 2xl:tw-h-[59.5vh] 3xl:tw-h-[60vh] tw-gap-10">
           <div className="tw-flex tw-flex-col tw-w-[40%] tw-justify-center tw-text-justify tw-h-full tw-items-center md:tw-items-start">
-            <div
-              id="hero-image"
+            <img
+              src={logo}
+              onMouseOver={(e) => (e.currentTarget.src = footerlogoicon)}
+              onMouseOut={(e) => (e.currentTarget.src = logo)}
+              alt=""
               className="tw-h-[190px] md:tw-h-[270px] xl:tw-h-[230px] 3xl:tw-h-[270px] tw-bg-contain xl:tw-bg-cover tw-mb-2 tw-w-full"
             />
             <div className="tw-text-sm md:tw-text-[13px] lg:tw-text-[12px] 2xl:tw-text-[15px] 3xl:tw-text-[18px] 3xl:tw-leading-7 tw-tracking-wide tw-text-white tw-font-semibold md:tw-mt-2 tw-mb-2">
@@ -42,7 +47,7 @@ export default function Hero() {
               You can gather your friends, form a team, battle with others and
               earn crypto with just your skills
             </div>
-            <div className="tw-text-sm 3xl:tw-text-[18px] 3xl:tw-mb-[4rem] tw-tracking-wide md:tw-text-base tw-text-[#00ffe0] tw-font-semibold tw-mb-4">
+            <div className="tw-text-sm 3xl:tw-text-[18px] 3xl:tw-mb-[3rem] tw-tracking-wide md:tw-text-base tw-text-[#00ffe0] tw-font-semibold tw-mb-4">
               #IntergalacticCockroach #ICC #SpaceRace # P2E #NFT
             </div>
             <iframe
@@ -50,9 +55,9 @@ export default function Hero() {
               title="icc video"
               src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1&loop=1&controls=0"
             ></iframe>
-            <div className="tw-text-base tw-font-semibold tw-mb-2 tw-text-white">
+            <div className="tw-flex tw-text-base tw-font-semibold tw-mb-2 tw-text-white tw-w-full">
               <span>$ICC Contract Address</span>
-              {copied && <span id="copyid">Copied</span>}
+              {copied && <span className="tw-ml-auto ">Copied</span>}
             </div>
             <div className="md:tw-hidden">
               <CopyToClipboard
@@ -70,7 +75,13 @@ export default function Hero() {
                 <img src={copy} alt="copy" />
               </CopyToClipboard>
             </div>
-            <div id="play-now-button" className="tw-h-[80px] tw-w-[100%]" />
+            <img
+              src={playNow}
+              onMouseOver={(e) => (e.currentTarget.src = playNowidlehover)}
+              onMouseOut={(e) => (e.currentTarget.src = playNow)}
+              alt=""
+              className="tw-h-[80px] tw-w-[100%] tw-mt-auto"
+            />
           </div>
           <div className="tw-hidden md:tw-flex tw-flex-col tw-flex-[1]">
             <iframe
@@ -80,18 +91,16 @@ export default function Hero() {
             ></iframe>
           </div>
         </div>
-        <div className="social-icons">
-          <div className="community-buttons">
-            <a href="#home">
-              <img src={opensea} alt="opensea" />
-            </a>
-            <a href="#home">
-              <img src={twitter} alt="discord" />
-            </a>
-            <a href="#home">
-              <img src={discord} alt="twitter" />
-            </a>
-          </div>
+        <div className="social-icons 3xl:tw-gap-[1.8rem]">
+          <a href="#home">
+            <img src={opensea} alt="opensea" />
+          </a>
+          <a href="#home">
+            <img src={twitter} alt="discord" />
+          </a>
+          <a href="#home">
+            <img src={discord} alt="twitter" />
+          </a>
         </div>
         <div id="mobile-nav">
           <img
