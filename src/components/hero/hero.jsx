384209384489327a48454423351ef4c17f7523ff
eliashezron <eliashezron23@gmail.com>
2022-07-18 10:13:20 +0300

    deploy

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -10,8 +10,6 @@ import earn from "../../assets/earnicon.png"
 import trade from "../../assets/tradeicon.png"
 import more from "../../assets/moreicon.png"
 import nft from "../../assets/nfticon.png"
-// import playNow from '../../assets/playnowidle.png';
-// import playNowidlehover from '../../assets/playnowidlehover.png';
 import CopyToClipboard from "react-copy-to-clipboard"
 
 import NavBar from "../navbar/navbar"
