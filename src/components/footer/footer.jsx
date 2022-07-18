9bddeed4f5fb7c7123c2b57a9962db055cb618b0
eliashezron <eliashezron23@gmail.com>
2022-07-18 09:55:41 +0300

    initial commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import './footer.scss';
-import logo from '../../assets/logo.png';
-import opensea from '../../assets/opensea.png';
-import discord from '../../assets/discord.png';
-import twitter from '../../assets/twitter.png';
-import mintnow from '../../assets/mintnow.svg';
-import mintnowhover from '../../assets/mintnowhover.svg';
-import playnow from '../../assets/playnowlong.svg';
-import playnowhover from '../../assets/playnowlonghover.svg';
-import iccprice from '../../assets/iccprice.svg';
-import buyicca from '../../assets/buyicca.svg';
-import bgc from '../../assets/bgc.png';
-import buyiccahover from '../../assets/buyiccahover.svg';
+import React from "react"
+import "./footer.scss"
+import logo from "../../assets/logo.png"
+import opensea from "../../assets/opensea.png"
+import discord from "../../assets/discord.png"
+import twitter from "../../assets/twitter.png"
+// import mintnow from '../../assets/mintnow.svg';
+// import mintnowhover from '../../assets/mintnowhover.svg';
+import playnow from "../../assets/playnowlong.svg"
+import playnowhover from "../../assets/playnowlonghover.svg"
+import iccprice from "../../assets/iccprice.svg"
+// import buyicca from '../../assets/buyicca.svg';
+import bgc from "../../assets/bgc.png"
+// import buyiccahover from '../../assets/buyiccahover.svg';
 export default function Footer() {
   return (
-    <section id="footer">
-      <div className="container">
+    <section id='footer'>
+      <div className='container'>
         {/* <div id="mobile-view">
           <img src={logo} alt="logo" id="logo" />
           <h1>2022@ INTERGALACTIC COCKROACH GAME. ALL RIGHTS RESERVED.</h1>
@@ -79,114 +79,114 @@ export default function Footer() {
           </div>
         </div> */}
         {/* ==================================================== */}
-        <div className="tw-flex tw-w-full tw-flex-row tw-gap-10 tw-mb-6">
+        <div className='tw-flex tw-w-full tw-flex-row tw-gap-10 tw-mb-6'>
           <div
-            className="tw-bg-center tw-bg-contain tw-bg-no-repeat tw-w-[600px] tw-h-[150px]"
+            className='tw-bg-center tw-bg-contain tw-bg-no-repeat tw-w-[600px] tw-h-[150px]'
             style={{
               backgroundImage: `url(${logo})`,
             }}
           />
-          <div className="tw-flex tw-w-full tw-flex-row">
-            <div className="tw-grid tw-grid-cols-3 tw-gap-1 tw-w-[65%]">
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+          <div className='tw-flex tw-w-full tw-flex-row'>
+            <div className='tw-grid tw-grid-cols-3 tw-gap-1 tw-w-[65%]'>
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Home
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Earn
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Trade
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 WhitePaper
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Farms
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Exchange
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Nfts
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Pools
               </button>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 Liquidity
               </button>
             </div>
-            <div className="tw-border-l-[1.5px] tw-mx-4 tw-border-[#0bfff0]" />
+            <div className='tw-border-l-[1.5px] tw-mx-4 tw-border-[#0bfff0]' />
 
-            <div className="tw-flex tw-flex-col tw-items-center tw-w-[40%] tw-gap-4">
+            <div className='tw-flex tw-flex-col tw-items-center tw-w-[40%] tw-gap-4'>
               <div
-                className="tw-bg-no-repeat tw-h-[83px] tw-w-full tw-rounded-[30px] tw-p-1 tw-bg-cover tw-bg-center tw-flex tw-flex-col tw-items-center tw-overflow-hidden tw-gap-1"
+                className='tw-bg-no-repeat tw-h-[83px] tw-w-full tw-rounded-[30px] tw-p-1 tw-bg-cover tw-bg-center tw-flex tw-flex-col tw-items-center tw-overflow-hidden tw-gap-1'
                 style={{
                   backgroundImage: `url(${bgc})`,
                 }}
               >
-                <div className="tw-font-semibold tw-text-[#fff060]">
+                <div className='tw-font-semibold tw-text-[#fff060]'>
                   Join our community
                 </div>
-                <div className="tw-flex tw-gap-[3rem]">
+                <div className='tw-flex tw-gap-[3rem]'>
                   <img
-                    className="tw-h-[40px] tw-w-[40px]"
+                    className='tw-h-[40px] tw-w-[40px]'
                     src={opensea}
-                    alt="opensea"
+                    alt='opensea'
                   />
 
                   <img
-                    className="tw-h-[40px] tw-w-[40px]"
+                    className='tw-h-[40px] tw-w-[40px]'
                     src={discord}
-                    alt="discord"
+                    alt='discord'
                   />
 
                   <img
-                    className="tw-h-[40px] tw-w-[40px]"
+                    className='tw-h-[40px] tw-w-[40px]'
                     src={twitter}
-                    alt="twitter"
+                    alt='twitter'
                   />
                 </div>
               </div>
-              <button className="tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-w-[50%] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0">
+              <button className='tw-p-[3px] tw-border-[#0bfff0] tw-border-b-[3px] tw-h-[50px] tw-w-[50%] tw-font-[Orbitron] tw-text-[0.65rem] tw-uppercase tw-text-[#0bfff0] tw-bg-transparent tw-rounded-3xl tw-border-t-0'>
                 contact us
               </button>
             </div>
           </div>
         </div>
-        <div className="tw-flex tw-w-full tw-justify-between tw-items-end">
-          <div className="tw-flex tw-flex-col tw-text-start tw-items-start tw-w-[60%]">
+        <div className='tw-flex tw-w-full tw-justify-between tw-items-end'>
+          <div className='tw-flex tw-flex-col tw-text-start tw-items-start tw-w-[60%]'>
             <img
-              className="tw-h-[80px] tw-w-[500px]"
+              className='tw-h-[80px] tw-w-[500px]'
               src={playnow}
-              alt="playnow"
+              alt='playnow'
               onMouseOver={(e) => (e.currentTarget.src = playnowhover)}
               onMouseOut={(e) => (e.currentTarget.src = playnow)}
             />
-            <div className="tw-text-[#fff060] tw-uppercase tw-text-base tw-font-extrabold">
+            <div className='tw-text-[#fff060] tw-uppercase tw-text-base tw-font-extrabold'>
               2022@ INTERGALACTIC COCKROACH GAME. ALL RIGHTS RESERVED.
             </div>
           </div>
-          <div className="tw-flex tw-flex-row tw-gap-2 tw-items-end tw-w-[40%]">
+          <div className='tw-flex tw-flex-row tw-gap-2 tw-items-end tw-w-[40%]'>
             <div
-              className="tw-bg-no-repeat tw-bg-center tw-bg-contain tw-h-[70px] tw-w-[280px]"
+              className='tw-bg-no-repeat tw-bg-center tw-bg-contain tw-h-[70px] tw-w-[280px]'
               style={{
                 backgroundImage: `url(${iccprice})`,
               }}
             />
-            <div className="tw-flex tw-flex-col">
+            <div className='tw-flex tw-flex-col'>
               <div
-                className="tw-bg-no-repeat tw-bg-center tw-bg-contain tw-h-[50px] tw-w-[200px]"
-                id="mint-button-desktop"
+                className='tw-bg-no-repeat tw-bg-center tw-bg-contain tw-h-[50px] tw-w-[200px]'
+                id='mint-button-desktop'
               ></div>
               <div
-                className="tw-bg-no-repeat tw-bg-center tw-bg-contain tw-h-[50px] tw-w-[200px]"
-                id="buy-button-desktop"
+                className='tw-bg-no-repeat tw-bg-center tw-bg-contain tw-h-[50px] tw-w-[200px]'
+                id='buy-button-desktop'
               ></div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,119 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import './hero.scss';
-import twitter from '../../assets/twittericon.png';
-import discord from '../../assets/discordicon.png';
-import opensea from '../../assets/openseaicon.png';
-import copy from '../../assets/iccaddress.png';
-import copyM from '../../assets/copymobiletoken.png';
-import home from '../../assets/homeicon.png';
-import earn from '../../assets/earnicon.png';
-import trade from '../../assets/tradeicon.png';
-import more from '../../assets/moreicon.png';
-import nft from '../../assets/nfticon.png';
+import React, { useState, useEffect } from "react"
+import "./hero.scss"
+import twitter from "../../assets/twittericon.png"
+import discord from "../../assets/discordicon.png"
+import opensea from "../../assets/openseaicon.png"
+import copy from "../../assets/iccaddress.png"
+import copyM from "../../assets/copymobiletoken.png"
+import home from "../../assets/homeicon.png"
+import earn from "../../assets/earnicon.png"
+import trade from "../../assets/tradeicon.png"
+import more from "../../assets/moreicon.png"
+import nft from "../../assets/nfticon.png"
 // import playNow from '../../assets/playnowidle.png';
 // import playNowidlehover from '../../assets/playnowidlehover.png';
-import CopyToClipboard from 'react-copy-to-clipboard';
+import CopyToClipboard from "react-copy-to-clipboard"
 
-import NavBar from '../navbar/navbar';
+import NavBar from "../navbar/navbar"
 
 export default function Hero() {
-  const [copied, setCopied] = useState(false);
+  const [copied, setCopied] = useState(false)
 
   useEffect(() => {
     if (copied) {
       setTimeout(() => {
-        setCopied(false);
-      }, [6000]);
+        setCopied(false)
+      }, [6000])
     }
-  }, [copied]);
+  }, [copied])
   return (
-    <section id="hero">
+    <section id='hero'>
       <NavBar />
-      <div id="hero">
-        <div className="tw-flex md:tw-flex-row tw-w-full tw-gap-10">
-          <div className="tw-flex tw-flex-col md:tw-flex-[1] tw-text-center md:tw-text-start">
-            <div id="hero-image" />
-            <div className="tw-text-sm md:tw-text-md tw-tracking-wide tw-text-white tw-font-semibold md:tw-mt-10 tw-my-2">
+      <div id='hero'>
+        <div className='tw-flex md:tw-flex-row tw-w-full tw-gap-10'>
+          <div className='tw-flex tw-flex-col md:tw-flex-[1] tw-text-center md:tw-text-start'>
+            <div id='hero-image' />
+            <div className='tw-text-sm md:tw-text-md tw-tracking-wide tw-text-white tw-font-semibold md:tw-mt-10 tw-my-2'>
               Intergalactic Cockroach Space Race is an esport game based on
               blockchain technology. <br></br>
               You can gather your friends, form a team, battle with others and
               earn crypto with just your skills
             </div>
-            <div className="tw-text-sm tw-tracking-wide md:tw-text-base tw-text-[#00ffe0] tw-font-semibold tw-mb-4">
+            <div className='tw-text-sm tw-tracking-wide md:tw-text-base tw-text-[#00ffe0] tw-font-semibold tw-mb-4'>
               #IntergalacticCockroach #ICC #SpaceRace # P2E #NFT
             </div>
             <iframe
-              className="mobile-iframe"
-              title="icc video"
-              src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1&loop=1&controls=0"
+              className='mobile-iframe'
+              title='icc video'
+              src='https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1&loop=1&controls=0'
             ></iframe>
-            <div className="tw-text-base tw-font-semibold tw-mb-2 tw-text-white">
+            <div className='tw-text-base tw-font-semibold tw-mb-2 tw-text-white'>
               <span>$ICC Contract Address</span>
-              {copied && <span id="copyid">Copied</span>}
+              {copied && <span id='copyid'>Copied</span>}
             </div>
-            <div className="md:tw-hidden">
+            <div className='md:tw-hidden'>
               <CopyToClipboard
-                text="0x8d672014fb107cb409dccd9042dda3b97313f4c3"
+                text='0x8d672014fb107cb409dccd9042dda3b97313f4c3'
                 onCopy={() => setCopied(true)}
               >
-                <img src={copyM} alt="copy" />
+                <img src={copyM} alt='copy' />
               </CopyToClipboard>
             </div>
-            <div className="tw-hidden md:tw-block">
+            <div className='tw-hidden md:tw-block'>
               <CopyToClipboard
-                text="0x8d672014fb107cb409dccd9042dda3b97313f4c3"
+                text='0x8d672014fb107cb409dccd9042dda3b97313f4c3'
                 onCopy={() => setCopied(true)}
               >
-                <img src={copy} alt="copy" />
+                <img src={copy} alt='copy' />
               </CopyToClipboard>
             </div>
-            <div id="play-now-button" />
+            <div id='play-now-button' />
           </div>
-          <div className="tw-hidden md:tw-flex tw-flex-col tw-flex-[1]">
+          <div className='tw-hidden md:tw-flex tw-flex-col tw-flex-[1]'>
             <iframe
-              className="tw-rounded-[25px] tw-hidden md:tw-block tw-h-full tw-w-full tw-border-[5px] tw-border-[#fbc4e6]"
-              title="icc video"
-              src="https://www.youtube.com/embed/HhIl_XJ-OGA?autoplay=1&mute=1&loop=1&controls=0"
+              className='tw-rounded-[25px] tw-hidden md:tw-block tw-h-full tw-w-full tw-border-[5px] tw-border-[#fbc4e6]'
+              title='icc video'
+              src='https://www.youtube.com/embed/HhIl_XJ-OGA?autoplay=1&mute=1&loop=1&controls=0'
             ></iframe>
           </div>
         </div>
-        <div className="social-icons">
-          <div className="community-buttons">
-            <a href="#home">
-              <img src={opensea} alt="opensea" />
+        <div className='social-icons'>
+          <div className='community-buttons'>
+            <a href='#home'>
+              <img src={opensea} alt='opensea' />
             </a>
-            <a href="#home">
-              <img src={twitter} alt="discord" />
+            <a href='#home'>
+              <img src={twitter} alt='discord' />
             </a>
-            <a href="#home">
-              <img src={discord} alt="twitter" />
+            <a href='#home'>
+              <img src={discord} alt='twitter' />
             </a>
           </div>
         </div>
-        <div id="mobile-nav">
+        <div id='mobile-nav'>
           <img
             src={home}
-            alt=""
-            onClick={() => window.location.replace('/#hero')}
+            alt=''
+            onClick={() => window.location.replace("/#hero")}
           />
           <img
             src={nft}
-            alt=""
-            onClick={() => window.location.replace('/#nft-genesis')}
+            alt=''
+            onClick={() => window.location.replace("/#nft-genesis")}
           />
           <img
             src={earn}
-            onClick={() => window.location.replace('/#game-play')}
-            alt=""
+            onClick={() => window.location.replace("/#game-play")}
+            alt=''
           />
           <img
             src={trade}
-            onClick={() => window.location.replace('/#coin')}
-            alt=""
+            onClick={() => window.location.replace("/#icc-coin")}
+            alt=''
           />
-          <img src={more} alt="" />
+          <img src={more} alt='' />
         </div>
       </div>
     </section>
-  );
+  )
 }
